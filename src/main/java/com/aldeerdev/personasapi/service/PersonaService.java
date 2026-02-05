d646911e755260487343f2d5d4469b262cb6a6a1
AldeerDev <aldeerdev@gmail.com>
2026-02-05 19:51:21 -0300

    Refactoriza PersonaSerivce para usar JPA

--- src/main/java/com/aldeerdev/personasapi/service/PersonaService.java
+++ src/main/java/com/aldeerdev/personasapi/service/PersonaService.java
@@ -1,6 +1,5 @@
 package com.aldeerdev.personasapi.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -8,31 +7,30 @@ import org.springframework.stereotype.Service;
 import com.aldeerdev.personasapi.dto.PersonaRequestDTO;
 import com.aldeerdev.personasapi.exception.PersonaNotFoundException;
 import com.aldeerdev.personasapi.model.Persona;
+import com.aldeerdev.personasapi.repository.PersonaRepository;
 
 @Service
 public class PersonaService {
 
-	private final List<Persona> personas = new ArrayList<Persona>();
+	private final PersonaRepository repository;
 
-	public PersonaService() {
-		personas.add(new Persona(1L, "Juan", 30));
-		personas.add(new Persona(2L, "Ana", 25));
-		personas.add(new Persona(3L, "Pedro", 40));
+	public PersonaService(PersonaRepository repository) {
+		this.repository = repository;
 	}
 
 	public List<Persona> obtenerPersonas() {
-		return personas;
+		return repository.findAll();
 	}
 
 	public Persona obtenerPersonaPorId(Long id) {
-		return personas.stream().filter(p -> p.getId().equals(id)).findFirst()
-				.orElseThrow(() -> new PersonaNotFoundException(id));
+		return repository.findById(id).orElseThrow(() -> new PersonaNotFoundException(id));
 	}
 
 	public Persona crearPersona(PersonaRequestDTO dto) {
-		Persona persona = new Persona(3L, dto.getNombre(), dto.getEdad());
-		personas.add(persona);
-		return persona;
+		Persona persona = new Persona();
+		persona.setNombre(dto.getNombre());
+		persona.setEdad(dto.getEdad());
+		return repository.save(persona);
 	}
 
 	public Persona actualizarPersona(Long id, Persona personaActualizada) {
@@ -43,7 +41,6 @@ public class PersonaService {
 	}
 
 	public void eliminarPersona(Long id) {
-		Persona persona = obtenerPersonaPorId(id);
-		personas.remove(persona);
+		repository.deleteById(id);
 	}
 }
