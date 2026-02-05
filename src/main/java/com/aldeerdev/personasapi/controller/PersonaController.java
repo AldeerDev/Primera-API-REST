720171c2695fc66f0e1b784a4326b8005fcd8ee0
AldeerDev <aldeerdev@gmail.com>
2026-02-05 10:53:41 -0300

    Agrega nuevo endpoint, devuelve JSON del objeto Persona

--- src/main/java/com/aldeerdev/personasapi/controller/PersonaController.java
+++ src/main/java/com/aldeerdev/personasapi/controller/PersonaController.java
@@ -3,11 +3,18 @@ package com.aldeerdev.personasapi.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.aldeerdev.personasapi.model.Persona;
+
 @RestController
 public class PersonaController {
 	
 	@GetMapping("/saludo")
 	public String saludo() {
 		return "Hola Spring Boot!";
+	}
+	
+	@GetMapping("/persona")
+	public Persona obtenerPersona() {
+		return new Persona(1L, "Juan", 30);
 	}
 }
